ee383e4f33491fd55f2b365896206acb28ff89e1
Yash Kumar Verma <yk.verma2000@gmail.com>
2020-05-26 01:26:12 +0530

    transfer server listening to server.ts

--- src/lib/shatabdi.ts
+++ src/lib/shatabdi.ts
@@ -32,18 +32,6 @@ class Shatabdi {
     this.router.register(methods.GET, pattern, ...args)
   }
 
-  public post(pattern: string, ...args: layer[]): void {
-    this.router.register(methods.POST, pattern, ...args)
-  }
-
-  public put(pattern: string, ...args: layer[]): void {
-    this.router.register(methods.PUT, pattern, ...args)
-  }
-
-  public delete(pattern: string, ...args: layer[]): void {
-    this.router.register(methods.DELETE, pattern, ...args)
-  }
-
   public engine(req: request, res: http.ServerResponse) {
     this.request = req
     this.response = new Response(req, res)
@@ -53,16 +41,16 @@ class Shatabdi {
     if (req.url !== '/favicon.ico') console.log(`request hit as ${req.method} on ${req.url}`)
   }
 
-  public listen(port: number, callback?: any): void {
-    try {
-      http.createServer((req, res) => this.engine(req as request, res as http.ServerResponse)).listen(port || this.port)
-      if (callback) {
-        callback()
-      }
-    } catch (e) {
-      console.log(e)
-    }
-  }
+  //   public listen(port: number, callback?: any): void {
+  //     try {
+  //       http.createServer((req, res) => this.engine(req as request, res as http.ServerResponse)).listen(port || this.port)
+  //       if (callback) {
+  //         callback()
+  //       }
+  //     } catch (e) {
+  //       console.log(e)
+  //     }
+  //   }
 }
 
 export default Shatabdi
